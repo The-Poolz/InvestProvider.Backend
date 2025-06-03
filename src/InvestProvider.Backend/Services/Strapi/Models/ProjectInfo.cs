ea751fa1353c1c2532f64a3a912468323d955915
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2025-06-03 15:51:25 +0300

    GenerateSignature: Returns PoolzBackId too (#14)
    * GenerateSignature: Returns PoolzBackId too
    
    * - fix
    
    * - fix
    
    * - fix

--- src/InvestProvider.Backend/Services/Strapi/Models/ProjectInfo.cs
+++ src/InvestProvider.Backend/Services/Strapi/Models/ProjectInfo.cs
@@ -5,6 +5,5 @@ namespace InvestProvider.Backend.Services.Strapi.Models;
 public class ProjectInfo(ProjectInfoResponse graphQlResponse)
 {
     public ComponentPhaseStartEndAmount? CurrentPhase => graphQlResponse.ProjectsInfo.ProjectPhases.FirstOrDefault(x => x.Start <= DateTime.UtcNow || x.Finish > DateTime.UtcNow);
-    public int PoolId { get; } = graphQlResponse.ProjectsInfo.PoolzBackId!.Value;
     public long ChainId { get; } = graphQlResponse.ProjectsInfo.ChainSetting.Chain.ChainId!.Value;
 }
