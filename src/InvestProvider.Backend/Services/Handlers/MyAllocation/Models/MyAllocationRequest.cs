e41c7e2a3a61c7a1c29aa45f04e772d1d0415af7
ArdenHide <ardenhide@gmail.com>
2025-06-09 16:49:15 +0300

    - make better `MyAllocationRequest`

--- src/InvestProvider.Backend/Services/Handlers/MyAllocation/Models/MyAllocationRequest.cs
+++ src/InvestProvider.Backend/Services/Handlers/MyAllocation/Models/MyAllocationRequest.cs
@@ -4,11 +4,12 @@ using Net.Web3.EthereumWallet;
 
 namespace InvestProvider.Backend.Services.Handlers.MyAllocation.Models;
 
-public class MyAllocationRequest : IRequest<MyAllocationResponse>
+[method: JsonConstructor]
+public class MyAllocationRequest(string projectId, EthereumAddress userAddress) : IRequest<MyAllocationResponse>
 {
     [JsonRequired]
-    public string ProjectId { get; set; } = null!;
+    public string ProjectId { get; } = projectId;
 
     [JsonRequired]
-    public EthereumAddress UserAddress { get; set; } = null!;
+    public EthereumAddress UserAddress { get; } = userAddress;
 }
