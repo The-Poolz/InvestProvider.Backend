4d114ccf7714688f860fea3c75914075cb001c38
ArdenHide <ardenhide@gmail.com>
2025-06-09 16:47:44 +0300

    Implement `MyAllocation`

--- src/InvestProvider.Backend/Models/LambdaRequest.cs
+++ src/InvestProvider.Backend/Models/LambdaRequest.cs
@@ -1,4 +1,5 @@
 ﻿using Net.Utils.ErrorHandler.Extensions;
+using InvestProvider.Backend.Services.Handlers.MyAllocation.Models;
 using InvestProvider.Backend.Services.Handlers.GenerateSignature.Models;
 using InvestProvider.Backend.Services.Handlers.AdminWriteAllocation.Models;
 
@@ -8,11 +9,13 @@ public class LambdaRequest
 {
     public GenerateSignatureRequest? GenerateSignature { get; set; }
     public AdminWriteAllocationRequest? AdminWriteAllocation { get; set; }
+    public MyAllocationRequest? MyAllocation { get; set; }
 
     public object HandlerRequest => this switch
     {
         { GenerateSignature: not null } => GenerateSignature,
         { AdminWriteAllocation: not null } => AdminWriteAllocation,
+        { MyAllocation: not null } => MyAllocation,
         _ => throw Error.INVALID_HANDLER_REQUEST.ToException()
     };
 }
---
+++ src/InvestProvider.Backend/Services/Handlers/MyAllocation/Models/MyAllocationRequest.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Newtonsoft.Json;
+using Net.Web3.EthereumWallet;
+
+namespace InvestProvider.Backend.Services.Handlers.MyAllocation.Models;
+
+public class MyAllocationRequest : IRequest<MyAllocationResponse>
+{
+    [JsonRequired]
+    public string ProjectId { get; set; } = null!;
+
+    [JsonRequired]
+    public EthereumAddress UserAddress { get; set; } = null!;
+}
