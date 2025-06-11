6c1fa38ef13ff1fef28f21b71e880304604c2d22
Stanislav Vysotskyi <66203238+ArdenHide@users.noreply.github.com>
2025-06-11 18:11:57 +0300

    GenerateSignature: Limit generate signature until phase is not started (#32)

--- src/InvestProvider.Backend/Services/Strapi/Models/ProjectInfo.cs
+++ src/InvestProvider.Backend/Services/Strapi/Models/ProjectInfo.cs
@@ -4,7 +4,7 @@ namespace InvestProvider.Backend.Services.Strapi.Models;
 
 public class ProjectInfo(ProjectInfoResponse graphQlResponse)
 {
-    public ComponentPhaseStartEndAmount? CurrentPhase => Phases.FirstOrDefault(x => x.Start <= DateTime.UtcNow || x.Finish > DateTime.UtcNow);
+    public ComponentPhaseStartEndAmount? CurrentPhase => Phases.FirstOrDefault(x => x.Start <= DateTime.UtcNow && x.Finish > DateTime.UtcNow);
     public ICollection<ComponentPhaseStartEndAmount> Phases => graphQlResponse.ProjectsInfo.ProjectPhases;
     public long ChainId { get; } = graphQlResponse.ProjectsInfo.ChainSetting.Chain.ChainId!.Value;
 }
