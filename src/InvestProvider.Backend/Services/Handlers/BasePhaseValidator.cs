13839fd5fce017de5ecb8d8f91b13fd1da8652ca
Stan Goldin <48094744+Lomet@users.noreply.github.com>
2025-06-22 15:21:01 +0300

    Add explicit usings to phase validator

--- src/InvestProvider.Backend/Services/Handlers/BasePhaseValidator.cs
+++ src/InvestProvider.Backend/Services/Handlers/BasePhaseValidator.cs
@@ -1,4 +1,7 @@
 using FluentValidation;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
     where T : IPhaseRequest
         model.PhaseContext.StrapiProjectInfo = _strapi.ReceiveProjectInfo(model.ProjectId, filterPhases: model.FilterPhases);
         return model.PhaseContext.StrapiProjectInfo.CurrentPhase != null;
