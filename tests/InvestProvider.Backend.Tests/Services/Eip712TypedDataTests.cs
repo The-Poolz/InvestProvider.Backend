a949304a2c34be95edb7615851b28d78803550fe
Stan Goldin <48094744+Lomet@users.noreply.github.com>
2025-06-22 17:33:04 +0300

    Add tests for Eip712TypedData

---
+++ tests/InvestProvider.Backend.Tests/Services/Eip712TypedDataTests.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+using Xunit;
+using Net.Web3.EthereumWallet;
+using InvestProvider.Backend.Services.Web3.Eip712.Models;
+
+namespace InvestProvider.Backend.Tests.Services;
+
+public class Eip712TypedDataTests
+{
+    [Fact]
+    public void ToEip712Json_ReturnsOrderedFieldsAndValues()
+    {
+        var domain = new Eip712Domain(5, new EthereumAddress("0x00000000000000000000000000000000000000aa"));
+        var message = new InvestMessage(
+            poolId: 1,
+            userAddress: new EthereumAddress("0x00000000000000000000000000000000000000bb"),
+            amount: new System.Numerics.BigInteger(10),
+            validUntil: DateTime.UnixEpoch,
+            nonce: new System.Numerics.BigInteger(3));
+
+        var typedData = new Eip712TypedData(domain, message);
+        var json = typedData.ToEip712Json();
+
+        var obj = JObject.Parse(json);
+
+        Assert.Equal(new[] { "types", "domain", "primaryType", "message" }, obj.Properties().Select(p => p.Name).ToArray());
+        Assert.Equal("InvestMessage", (string)obj["primaryType"]!);
+        Assert.Equal(5, (long)obj["domain"]!["chainId"]!);
+        Assert.Equal("0x00000000000000000000000000000000000000aa", (string)obj["domain"]!["verifyingContract"]!);
+        Assert.Equal(1, (int)obj["message"]!["poolId"]!);
+        Assert.Equal("0x00000000000000000000000000000000000000bb", (string)obj["message"]!["user"]!);
+        Assert.Equal(10, (int)obj["message"]!["amount"]!);
+        Assert.Equal(0, (int)obj["message"]!["validUntil"]!);
+        Assert.Equal(3, (int)obj["message"]!["nonce"]!);
+    }
+}
