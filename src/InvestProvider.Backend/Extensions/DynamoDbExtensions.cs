46bd5c9fde6913149cd842d13f7b96b9c8ebe7a0
Stan Goldin <48094744+Lomet@users.noreply.github.com>
2025-06-22 17:43:36 +0300

    Refactor batch load extension for testability (#73)

--- src/InvestProvider.Backend/Extensions/DynamoDbExtensions.cs
+++ src/InvestProvider.Backend/Extensions/DynamoDbExtensions.cs
@@ -1,14 +1,15 @@
 ﻿using Amazon.DynamoDBv2.DataModel;
+using System.Linq;
 
 namespace InvestProvider.Backend.Extensions;
 
 public static class DynamoDbExtensions
 {
     public static async Task<List<T>> BatchLoadAsync<T>(this IDynamoDBContext dynamoDb, IEnumerable<object> keys, CancellationToken ct = default)
+        where T : class
     {
-        var batch = dynamoDb.CreateBatchGet<T>();
-        foreach (var k in keys) batch.AddKey(k);
-        await batch.ExecuteAsync(ct);
-        return batch.Results;
+        var tasks = keys.Select(k => dynamoDb.LoadAsync<T>(k, ct));
+        var results = await Task.WhenAll(tasks);
+        return results.Where(r => r != null).ToList()!;
     }
 }
