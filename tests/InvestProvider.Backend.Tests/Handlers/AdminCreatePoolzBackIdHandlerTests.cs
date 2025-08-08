218e002cab84f21f5ad6db353ba7f831cae1e873
ArdenHide <ardenhide@gmail.com>
2025-08-08 13:35:17 +0300

    - update tests

--- tests/InvestProvider.Backend.Tests/Handlers/AdminCreatePoolzBackIdHandlerTests.cs
+++ tests/InvestProvider.Backend.Tests/Handlers/AdminCreatePoolzBackIdHandlerTests.cs
@@ -1,11 +1,20 @@
-using System.Threading;
-using System.Threading.Tasks;
 using Moq;
 using Xunit;
+using System;
+using System.Numerics;
+using System.Threading;
+using System.Threading.Tasks;
+using Nethereum.RPC.Eth.DTOs;
+using Net.Web3.EthereumWallet;
+using Net.Cache.DynamoDb.ERC20;
 using Amazon.DynamoDBv2.DataModel;
+using Net.Cache.DynamoDb.ERC20.Models;
+using InvestProvider.Backend.Services.Strapi;
+using poolz.finance.csharp.contracts.LockDealNFT;
+using InvestProvider.Backend.Services.Strapi.Models;
+using InvestProvider.Backend.Services.Web3.Contracts;
 using InvestProvider.Backend.Services.Handlers.AdminCreatePoolzBackId;
 using InvestProvider.Backend.Services.Handlers.AdminCreatePoolzBackId.Models;
-using InvestProvider.Backend.Tests;
 
 namespace InvestProvider.Backend.Tests.Handlers;
 
@@ -14,10 +23,32 @@ public class AdminCreatePoolzBackIdHandlerTests
     [Fact]
     public async Task Handle_SavesItem_AndReturnsResponse()
     {
+        Environment.SetEnvironmentVariable("AWS_REGION", "us-east-1");
+
         var phase = TestHelpers.CreatePhase("1", System.DateTime.UtcNow, System.DateTime.UtcNow.AddHours(1), 0m);
         var projectInfo = TestHelpers.CreateProjectInfo(1, phase);
         var dynamoDb = new Mock<IDynamoDBContext>();
-        var handler = new AdminCreatePoolzBackIdHandler(dynamoDb.Object);
+        var lockDealNFT = new Mock<ILockDealNFTService<ContractType>>();
+        lockDealNFT.Setup(x => x.TokenOfQueryAsync(It.IsAny<long>(), ContractType.LockDealNFT, It.IsAny<BigInteger>(), It.IsAny<BlockParameter>()))
+            .ReturnsAsync(new EthereumAddress("0x00000000000000000000000000000000000000aa"));
+
+        var strapi = new Mock<IStrapiClient>();
+        strapi.Setup(x => x.ReceiveOnChainInfoAsync(1))
+            .ReturnsAsync(new OnChainInfo(
+                "http://rpc",
+                new EthereumAddress("0x00000000000000000000000000000000000000bb"),
+                new EthereumAddress("0x00000000000000000000000000000000000000cc")
+            ));
+
+        var erc20 = new Mock<ERC20CacheProvider>();
+        erc20.Setup(x => x.GetOrAdd(It.IsAny<GetCacheRequest>()))
+            .Returns(new ERC20DynamoDbTable());
+
+        var handler = new AdminCreatePoolzBackIdHandler(
+            dynamoDb.Object,
+            lockDealNFT.Object,
+            strapi.Object,
+            erc20.Object);
         var request = new AdminCreatePoolzBackIdRequest
         {
             ProjectId = "pid",
