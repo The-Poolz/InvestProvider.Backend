82aa39901f3fe60e57ead48bf2937ca61fe30e32
ArdenHide <ardenhide@gmail.com>
2025-11-12 13:05:13 +0300

    - refactor

--- src/InvestProvider.Backend/Services/Strapi/Models/ProjectInfo.cs
+++ src/InvestProvider.Backend/Services/Strapi/Models/ProjectInfo.cs
@@ -1,20 +1,27 @@
 ﻿using Poolz.Finance.CSharp.Strapi;
+using Net.Utils.ErrorHandler.Extensions;
 
 namespace InvestProvider.Backend.Services.Strapi.Models;
 
 public class ProjectInfo
 {
-    public ProjectInfo(ProjectInfoResponse graphQlResponse)
+    public ProjectInfo(ProjectInfoResponse graphQlResponse, string projectId)
     {
         ArgumentNullException.ThrowIfNull(graphQlResponse);
 
         var projectsInformation = graphQlResponse.ProjectsInfo
-            ?? throw new InvalidOperationException("Project information is missing in Strapi response.");
+            ?? throw Error.STRAPI_MISSING_PROJECT_DATA.ToException(new
+            {
+                ProjectId = projectId
+            });
 
         Phases = projectsInformation.ProjectPhases ?? Array.Empty<ComponentPhaseStartEndAmount>();
 
         ChainId = projectsInformation.ChainSetting?.Chain?.ChainId
-            ?? throw new InvalidOperationException("ChainSetting/Chain is missing in Strapi response to receive project ChainId.");
+            ?? throw Error.STRAPI_MISSING_CHAIN_DATA.ToException(new
+            {
+                ProjectId = projectId
+            });
     }
 
     public ComponentPhaseStartEndAmount? CurrentPhase => Phases.Count == 0 ? null : Phases.First();
