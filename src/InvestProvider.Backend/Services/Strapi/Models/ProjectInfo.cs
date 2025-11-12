569364408944dbc287821cae9bb991390bd6eba5
ArdenHide <ardenhide@gmail.com>
2025-11-12 11:34:22 +0300

    Better error handling if Strapi return incomplete data

--- src/InvestProvider.Backend/Services/Strapi/Models/ProjectInfo.cs
+++ src/InvestProvider.Backend/Services/Strapi/Models/ProjectInfo.cs
@@ -2,9 +2,23 @@
 
 namespace InvestProvider.Backend.Services.Strapi.Models;
 
-public class ProjectInfo(ProjectInfoResponse graphQlResponse)
+public class ProjectInfo
 {
+    public ProjectInfo(ProjectInfoResponse graphQlResponse)
+    {
+        ArgumentNullException.ThrowIfNull(graphQlResponse);
+
+        var projectsInformation = graphQlResponse.ProjectsInfo ?? throw new InvalidOperationException("Project information is missing in Strapi response.");
+
+        Phases = projectsInformation.ProjectPhases ?? Array.Empty<ComponentPhaseStartEndAmount>();
+
+        ChainId = projectsInformation.ChainSetting?.Chain?.ChainId
+                  ?? throw new InvalidOperationException("ChainSetting/Chain is missing in Strapi response to receive project ChainId.");
+    }
+
     public ComponentPhaseStartEndAmount? CurrentPhase => Phases.Count == 0 ? null : Phases.First();
-    public ICollection<ComponentPhaseStartEndAmount> Phases => graphQlResponse.ProjectsInfo.ProjectPhases;
-    public long ChainId { get; } = graphQlResponse.ProjectsInfo.ChainSetting.Chain.ChainId!.Value;
+
+    public ICollection<ComponentPhaseStartEndAmount> Phases { get; }
+
+    public long ChainId { get; }
 }
