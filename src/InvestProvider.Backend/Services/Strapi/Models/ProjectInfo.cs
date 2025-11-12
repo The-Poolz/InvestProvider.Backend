8a337117b4316146e7200450bcee447a91a71245
ArdenHide <ardenhide@gmail.com>
2025-11-12 11:34:43 +0300

    - tabs

--- src/InvestProvider.Backend/Services/Strapi/Models/ProjectInfo.cs
+++ src/InvestProvider.Backend/Services/Strapi/Models/ProjectInfo.cs
@@ -8,12 +8,13 @@ public class ProjectInfo
     {
         ArgumentNullException.ThrowIfNull(graphQlResponse);
 
-        var projectsInformation = graphQlResponse.ProjectsInfo ?? throw new InvalidOperationException("Project information is missing in Strapi response.");
+        var projectsInformation = graphQlResponse.ProjectsInfo
+            ?? throw new InvalidOperationException("Project information is missing in Strapi response.");
 
         Phases = projectsInformation.ProjectPhases ?? Array.Empty<ComponentPhaseStartEndAmount>();
 
         ChainId = projectsInformation.ChainSetting?.Chain?.ChainId
-                  ?? throw new InvalidOperationException("ChainSetting/Chain is missing in Strapi response to receive project ChainId.");
+            ?? throw new InvalidOperationException("ChainSetting/Chain is missing in Strapi response to receive project ChainId.");
     }
 
     public ComponentPhaseStartEndAmount? CurrentPhase => Phases.Count == 0 ? null : Phases.First();
